a6cb17a3b2d026e045de2b54f299c099ba6c7ab8
gleb89 <glebhleb89@icloud.com>
2021-06-15 00:43:23 +0300

    fff

--- front/components/Info.vue
+++ front/components/Info.vue
@@ -78,7 +78,7 @@ import Comments from "@/components/Comments";
 export default {
   async fetch() {
     this.comments = await fetch(
-      `httpa://apicrypto.ru/api/v1/comments/?coin_id=${this.coin.id}`
+      `https://apicrypto.ru/api/v1/comments/?coin_id=${this.coin.id}`
     ).then((res) => res.json());
   },
   props: ["coin", "onsendComentParent"],
@@ -140,7 +140,7 @@ export default {
       };
      
       this.$axios
-        .$post("https://apicrypto.ru/api/v1/comments/", data, {
+        .$post("https://apicrypto.ru/api/v1/comments", data, {
           headers: headers
         })
         .then(
